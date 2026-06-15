0a31e0fc8289dafcf8b30a9398ad6a0f2a47fc90
VirginiaR <7237305+virginiar@users.noreply.github.com>
2026-06-15 04:05:26 -0400

    Task 3
    Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,11 @@
+# Import json library to format responses
+import json
 # Import the requests library to handle HTTP requests
 import requests
 
 def emotion_detector(text_to_analyze):
     """
-    Function ro call Emotion Detection function of Watson NLP library.
+    Function to call Emotion Detection function of Watson NLP library.
     """
     # Required parameters
     url = "https://sn-watson-emotion.labs.skills.network/v1/" \
@@ -12,5 +14,18 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
     # Send POST request to emotion detection
     response = requests.post(url, json=input_json, headers=headers, timeout=10)
-    # Return the text attribute of the response
-    return response.text
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    # Extract the required set of emotions
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    # Find the dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    # Return the formatted response
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
